b058e73353bf19c4b014db47afdf497b2e5171e4
JZamboni <jacopozamboni@gmail.com>
2016-06-26 11:50:51 +0200

    risolvimento problemi

--- Example/jsonEx.py
+++ Example/jsonEx.py
@@ -1,4 +1,5 @@
-from Handler.IOPorters.jsonIn import Json as VarImporter
+from Handler.IOPorters.jsonIn import JsonIn as VarImporter
+import json
 
 myImporter = VarImporter(Component='Fuselage',
                          VariableName='fuselage',
@@ -7,4 +8,24 @@ myImporter = VarImporter(Component='Fuselage',
 
 value = VarImporter.finder(myImporter)
 
-print value
+Performance = {
+    "Mach cruise": 0.77,
+    "Wing Loading": 5000.0,
+    "MTOW": 422713.0,
+    "Cruise Altitude": 10000.0
+}
+
+Configuration = {
+    "Tail Type": "T tail",
+    "Engine Position": "wing",
+    "Wing Position": "low wing"
+}
+
+fileJ = []
+
+fileJ.append(Performance)
+fileJ.append(Configuration)
+
+fout = open('C:\Users\Jacopo\Desktop\Academic\GitHub\KBE_Retake\Output\jsonfile.json', 'w')
+
+json.dump(fileJ, fout, indent=4, sort_keys=True)
